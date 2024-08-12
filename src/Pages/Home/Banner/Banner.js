ba740c0d70fddabc7b9bb725212363e8d4ade7a4
Dilip Sarker <dilipsarker719@gmail.com>
2024-08-12 20:33:13 +0600

    navbar updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,32 +1,33 @@
-import React from "react";
-import chairImage from "../../../assets/images/chair.png";
-import backgroundImage from "../../../assets/images/bg.png";
-import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
+import React from 'react';
+import chairImage from '../../../assets/images/chair.png';
+import backgroundImage from '../../../assets/images/bg.png';
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
 const Banner = () => {
-  return (
-    <div
-      className="hero"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={chairImage}
-          alt="chair"
-          className="lg:w-1/2 rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-4xl font-medium">
-            Make your perfect smile event better!
-          </h1>
-          <p className="py-6">
-            Protect your smile and keep it healthy, with the largest network of
-            dentist nationwide, quick service and personalized service.
-          </p>
-          <PrimaryButton>Get Started</PrimaryButton>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="hero"
+			style={{ backgroundImage: `url(${backgroundImage})` }}
+		>
+			<div className="hero-content flex-col lg:flex-row-reverse">
+				<img
+					src={chairImage}
+					alt="chair"
+					className="lg:w-1/2 rounded-lg shadow-2xl"
+				/>
+				<div>
+					<h1 className="text-4xl font-medium">
+						Make your perfect smile event better!
+					</h1>
+					<p className="py-6">
+						Protect your smile and keep it healthy, with the largest network of
+						dentist nationwide, quick service and personalized service.
+					</p>
+					<p className="py-6"></p>
+					<PrimaryButton>Get Started</PrimaryButton>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
